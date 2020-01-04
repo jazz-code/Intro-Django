b4de92268bdd9f51d114420adfa285b6a4af5bb9
ren.meta@gmail.com <ren.meta@gmail.com>
2020-01-04 10:45:12 -0500

    filter results by user

--- notes/api.py
+++ notes/api.py
@@ -10,12 +10,20 @@ class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
         return note
 
         # debugger
-        import pdb; pdb.set_trace()
-        pass
+        # import pdb; pdb.set_trace()
+        # pass
     class Meta:
         model = PersonalNote
         fields = ('title', 'content')
 
 class PersonalNoteViewSet(viewsets.ModelViewSet):
     serializer_class = PersonalNoteSerializer
-    queryset = PersonalNote.objects.all()
+    queryset = PersonalNote.objects.none()
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_anonymous:
+            return PersonalNote.objects.none()
+        else:
+            return PersonalNote.objects.filter(user=user)
